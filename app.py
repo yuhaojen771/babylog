331ce38324d5e84f9936e077c753d26b058ed826
yuhaojen771 <perhaps771@gmail.com>
2025-06-02 17:38:13 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ app = Flask(__name__)
 CHANNEL_ACCESS_TOKEN = os.environ.get("0UR4cpzlLkEIBNIWBcx1XEKiXJPVbOlrxILQCoBOd4Hq2IVd8oLCq+kNswm+JR0Q7jj26lNkMCkq1eGkNhA6FAMRMLQigLE1DDlKJ/Rd8NaO+Ax3SS78WxJ8dFLn7hgPE8uDXe1urmNhq3MAyBKF8gdB04t89/1O/w1cDnyilFU=")
 CHANNEL_SECRET = os.environ.get("287dd374c6ee48c3b7b4239d70571c47")
 
+if not CHANNEL_ACCESS_TOKEN or not CHANNEL_SECRET:
+    raise ValueError("請設定 CHANNEL_ACCESS_TOKEN 和 CHANNEL_SECRET 環境變數")
+
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
 
